d903ea95c48c5bde559a06840ae44894142c862e
spot360 <spot360.team@gmail.com>
2016-07-05 16:02:08 +0900

    dist폴더 추가

--- woochang_erp/settings.py
+++ woochang_erp/settings.py
@@ -122,11 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-# # STATIC_URL = os.path.join(BASE_DIR, '/static/')
+# STATIC_URL = os.path.join(BASE_DIR, '/static/')
 
-# STATIC_ROOT = ''
-# STATICFILES_FINDERS = (
-#     'django.contrib.staticfiles.finders.FileSystemFinder',
-#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-# #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-# )
+STATIC_ROOT = []
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+   'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
